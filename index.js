1dbd9e0e0e25f827e616a514e11290787bf19ea6
Yanzen <yanwarsolah@gmail.com>
2022-09-20 20:09:02 +0700

    kirim data pug

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var app = express();
 
 app.set("view engine", "pug");
 app.set("views", "./views");
+app.use("/public", express.static("public"));
+app.use("/images", express.static("images"));
 
 app.get("/", function (req, res) {
   let data = {
     nama: "Yanwar",
     email: "yanwarsolah@gmail.com",
     status: true,
+    content: "konten",
     hobi: ["tidur", "makan", "jail", "baca", "nonton spoiler"],
   };
 
