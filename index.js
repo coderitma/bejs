8805f37933bf7d029a06b2dd1118ef70b2f13070
Yanzen <yanwarsolah@gmail.com>
2022-09-20 19:43:00 +0700

    pug 3: render

--- index.js
+++ index.js
@@ -5,35 +5,14 @@ app.set("view engine", "pug");
 app.set("views", "./views");
 
 app.get("/", function (req, res) {
-  res.render("index");
+  let data = {
+    nama: "Yanwar",
+    email: "yanwarsolah@gmail.com",
+    status: true,
+    hobi: ["tidur", "makan", "jail", "baca", "nonton spoiler"],
+  };
+
+  res.render("index", { data });
 });
-// var mahasiswaController = require("./controllers/mahasiswa");
-// var produkController = require("./controllers/produk");
-
-// app.use(function (req, res, next) {
-//   console.log(req.params);
-//   if (req.params && req.params.nim) {
-//     console.log("middleware nim = " + req.params.nim);
-//   }
-//   console.log("middleware nim");
-//   next();
-// });
-
-// app.get("/", function (req, res, next) {
-//   res.send("ulala");
-//   next();
-// });
-
-// app.use("/mahasiswa", mahasiswaController);
-// app.use("/produk", produkController);
-
-// app.use(function (req, res) {
-//   console.log("middleware response");
-// });
-
-// app.get("*", function (req, res, next) {
-//   // TODO: res.write
-//   res.status(404).send("404 Not Found!");
-// });
 
 app.listen(3001);
